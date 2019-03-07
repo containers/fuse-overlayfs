f17f15a48bebdc07f565def795562ddc57c7166c
Giuseppe Scrivano <gscrivan@redhat.com>
2019-03-07 10:22:26 +0100

    build: provide replacement for TEMP_FAILURE_RETRY if not present
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- main.c
+++ main.c
@@ -73,6 +73,16 @@
 
 #include <utils.h>
 
+#ifndef TEMP_FAILURE_RETRY
+#define TEMP_FAILURE_RETRY(expression) \
+  (__extension__                                                              \
+    ({ long int __result;                                                     \
+       do __result = (long int) (expression);                                 \
+       while (__result == -1L && errno == EINTR);                             \
+       __result; }))
+#endif
+
+
 #ifndef HAVE_OPEN_BY_HANDLE_AT
 struct file_handle
 {
