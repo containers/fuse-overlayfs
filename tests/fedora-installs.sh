e9f5f04883f713dc08f24bf1ea5af743d7cf661e
Giuseppe Scrivano <gscrivan@redhat.com>
2019-06-12 12:13:58 +0200

    rm: fix regression when deleting a file
    fix a regression caused by 7dcef0769b1521b8f14bdee58339c26140466c85
    
    Closes: https://bodhi.fedoraproject.org/updates/FEDORA-2019-fff1ded16e#comment-958825
    
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- tests/fedora-installs.sh
+++ tests/fedora-installs.sh
@@ -1,10 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+
+set -xeuo pipefail
 
 mkdir lower upper workdir merged
 
 fuse-overlayfs -o lowerdir=lower,upperdir=upper,workdir=workdir,suid,dev merged
 
-docker run --rm -ti -v merged:/merged fedora dnf --installroot /merged --releasever 29 install -y glibc-common gedit
+docker run --rm -ti -v merged:/merged fedora dnf --installroot /merged --releasever 30 install -y glibc-common gedit
 
 umount merged
 
@@ -16,8 +18,11 @@ mkdir upper workdir
 fuse-overlayfs -o lowerdir=lower,upperdir=upper,workdir=workdir,suid,dev merged
 
 # Install some big packages
-docker run --rm -ti -v merged:/merged fedora dnf --installroot /merged --releasever 29 install -y emacs texlive
+docker run --rm -ti -v merged:/merged fedora dnf --installroot /merged --releasever 30 install -y emacs texlive
 
-docker run --rm -ti -v merged:/merged fedora sh -c 'rm /usr/share/glib-2.0/schemas/gschemas.compiled; glib-compile-schemas /usr/share/glib-2.0/schemas/'
+docker run --rm -ti -v merged:/merged fedora sh -c 'rm /merged/usr/share/glib-2.0/schemas/gschemas.compiled; glib-compile-schemas /merged/usr/share/glib-2.0/schemas/'
 
 umount merged
+fuse-overlayfs -o lowerdir=lower,upperdir=upper,workdir=workdir,suid,dev merged
+
+docker run --rm -ti -v merged:/merged fedora sh -c 'rm -rf /merged/usr/share/glib-2.0/'
