9b687001bba8a39fc602f68c7ad0d49a0bd5c262
Giuseppe Scrivano <gscrivan@redhat.com>
2020-04-30 11:14:00 +0200

    main: fix signature for ovl_ioctl
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- main.c
+++ main.c
@@ -4694,7 +4694,7 @@ direct_ioctl (struct ovl_layer *l, int fd, int cmd, unsigned long *r)
 }
 
 static void
-ovl_ioctl (fuse_req_t req, fuse_ino_t ino, unsigned int cmd, void *arg,
+ovl_ioctl (fuse_req_t req, fuse_ino_t ino, int cmd, void *arg,
            struct fuse_file_info *fi, unsigned int flags,
            const void *in_buf, size_t in_bufsz, size_t out_bufsz)
 {
